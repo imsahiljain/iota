9bd9a289fbd52eb461da4eb96c927f43e14e518e
imsahiljain <emailofsahiljain@gmail.com>
2022-06-23 12:45:27 +0530

    add specs page

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,39 @@
 function toggleMobileMenu(menu) {
   menu.classList.toggle("open");
 }
+
+// POPUPS
+function Popup() {
+  var popup = document.getElementById("popup");
+  popup.classList.toggle("show");
+}
+
+function Popup2() {
+  var popup = document.getElementById("popup2");
+  popup.classList.toggle("show");
+}
+
+function Popup3() {
+  var popup = document.getElementById("popup3");
+  popup.classList.toggle("show");
+}
+
+function Popup4() {
+  var popup = document.getElementById("popup4");
+  popup.classList.toggle("show");
+}
+
+function Popup5() {
+  var popup = document.getElementById("popup5");
+  popup.classList.toggle("show");
+}
+
+function Popup6() {
+  var popup = document.getElementById("popup6");
+  popup.classList.toggle("show");
+}
+
+function Popup7() {
+  var popup = document.getElementById("popup7");
+  popup.classList.toggle("show");
+}
